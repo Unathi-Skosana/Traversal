97dc7d7a43d07a36b089a54a1943995fd1c76bf0
Verb656 <codename.unathi@gmail.com>
2016-10-14 09:30:52 +0200

    Added skeleton methods to gameBoard.java

---
+++ gameBoard.java
@@ -0,0 +1,28 @@
+public class gameBoard {
+
+    public final int boardWidth;
+    public final int boardHeight;
+
+    public gameBoard(int boardWidth, int boardHeight) {
+    
+        this.boardHeight = boardHeight;
+        this.boardWidth  = boardWidth;
+    }
+
+    public void add_gamePiece(gamePiece piece) {
+        
+        // add the game piece to whatever implementation,
+        //  you are using to represent gameBoard
+    }
+
+    public void draw_gamePiece(String symbol) {
+    
+      // draw gamePiece by using the graphics library.
+     
+    }
+
+
+
+
+
+}
