79c3b07f3cf1eb741f2db1b389d739543ad5fff7
Verb656 <codename.unathi@gmail.com>
2016-10-14 22:03:16 +0200

    Changed arguments of methods to the data typ[e Position

--- gamepiecesGUI.java
+++ gamepiecesGUI.java
@@ -9,237 +9,237 @@ public class gamepiecesGUI {
   private static final Color blue = new Color(0, 76, 255);
   private static final Color greenK = new Color(24, 219, 190);
 
-    public static void player(int i, int j) {    
+    public static void player(Position p) {    
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledCircle(i + .52,j + .52,.40);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.40);
         StdDraw.setPenColor(StdDraw.WHITE);
-        StdDraw.filledCircle(i + .52,j + .52,.325);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.325);
         StdDraw.setPenRadius(.0145);
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.line(i + .52,j + .72,i + .52,j + .32);
-        StdDraw.line(i + .72,j + .52,i + .32,j + .52);
+        StdDraw.line(p.getX() + .52,p.getY() + .72,p.getX() + .52,p.getY() + .32);
+        StdDraw.line(p.getX() + .72,p.getY() + .52,p.getX() + .32,p.getY() + .52);
     }
 
-    public static void closed_Port(int i, int j) {
+    public static void closed_Port(Position p) {
         StdDraw.setPenColor(StdDraw.GRAY);
-        StdDraw.filledCircle(i + .52, j + .52, .40);
+        StdDraw.filledCircle(p.getX() + .52, p.getY() + .52, .40);
         StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
-        StdDraw.filledCircle(i + .52, j + .52, .325);
+        StdDraw.filledCircle(p.getX() + .52, p.getY() + .52, .325);
         StdDraw.setPenRadius(.0145);
         StdDraw.setPenColor(StdDraw.GRAY);
-        StdDraw.line(i + .36,j + .35,i + .67,j + .67);
-        StdDraw.line(i + .36,j + .67,i + .67,j + .35);
+        StdDraw.line(p.getX() + .36,p.getY() + .35,p.getX() + .67,p.getY() + .67);
+        StdDraw.line(p.getX() + .36,p.getY() + .67,p.getX() + .67,p.getY() + .35);
     }
 
-	  public static void target(int i, int j) {
+	  public static void target(Position p) {
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledCircle(i + .52, j + .52, .40);
+        StdDraw.filledCircle(p.getX() + .52, p.getY() + .52, .40);
         StdDraw.setPenColor(greenK);
-        StdDraw.filledCircle(i + .52,j + .52, .325);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52, .325);
         StdDraw.setPenRadius(.0145);
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.line(i + .335,j + .49,i + .431,j + .37);
-        StdDraw.line(i + .431,j + .37,i + .700,j + .64);
+        StdDraw.line(p.getX() + .335,p.getY() + .49,p.getX() + .431,p.getY() + .37);
+        StdDraw.line(p.getX() + .431,p.getY() + .37,p.getX() + .700,p.getY() + .64);
     }
 
-	  public static void vert_UpMover(int i, int j) {
+	  public static void vert_UpMover(Position p) {
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledCircle(i + .52, j + .52, .40);
+        StdDraw.filledCircle(p.getX() + .52, p.getY() + .52, .40);
         StdDraw.setPenColor(blue);
-        StdDraw.filledCircle(i + .52,j + .52, .325);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52, .325);
         StdDraw.setPenRadius(.0145);
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.line(i + .52,j + .845,i + .52,j + .75);
-        StdDraw.line(i + .52,j + .195,i + .52,j + .29);
-        StdDraw.line(i + .52,j + .600,i + .40,j + .45);
-        StdDraw.line(i + .52,j + .600,i + .64,j + .45);
+        StdDraw.line(p.getX() + .52,p.getY() + .845,p.getX() + .52,p.getY() + .75);
+        StdDraw.line(p.getX() + .52,p.getY() + .195,p.getX() + .52,p.getY() + .29);
+        StdDraw.line(p.getX() + .52,p.getY() + .600,p.getX() + .40,p.getY() + .45);
+        StdDraw.line(p.getX() + .52,p.getY() + .600,p.getX() + .64,p.getY() + .45);
     }
 
-	  public static void hor_UpMover(int i,int j) {
+	  public static void hor_UpMover(Position p) {
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledCircle(i + .52,j + .52, .40);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52, .40);
         StdDraw.setPenColor(blue);
-        StdDraw.filledCircle(i + .52, j + .52, .325);
+        StdDraw.filledCircle(p.getX() + .52, p.getY() + .52, .325);
         StdDraw.setPenRadius(0.0145);
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.line(i + .520,j + .60,i + .40,j + .45);
-        StdDraw.line(i + .520,j + .60,i + .64,j + .45);
-        StdDraw.line(i + .845,j + .52,i + .75,j + .52);
-        StdDraw.line(i + .195,j + .52,i + .29,j + .52);
+        StdDraw.line(p.getX() + .520,p.getY() + .60,p.getX() + .40,p.getY() + .45);
+        StdDraw.line(p.getX() + .520,p.getY() + .60,p.getX() + .64,p.getY() + .45);
+        StdDraw.line(p.getX() + .845,p.getY() + .52,p.getX() + .75,p.getY() + .52);
+        StdDraw.line(p.getX() + .195,p.getY() + .52,p.getX() + .29,p.getY() + .52);
     }
 
- 	  public static void hor_DownMover(int i,int j) {
+ 	  public static void hor_DownMover(Position p) {
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledCircle(i + .52,j + .52, .40);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52, .40);
         StdDraw.setPenColor(darkOrange);
-        StdDraw.filledCircle(i + .52,j + .52, .325);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52, .325);
         StdDraw.setPenRadius(.0145);
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.line(i + .520,j + .42,i + .40,j + .59);
-        StdDraw.line(i + .520,j + .42,i + .62,j + .59);
-        StdDraw.line(i + .845,j + .52,i + .75,j + .52);
-        StdDraw.line(i + .195,j + .52,i + .29,j + .52);
+        StdDraw.line(p.getX() + .520,p.getY() + .42,p.getX() + .40,p.getY() + .59);
+        StdDraw.line(p.getX() + .520,p.getY() + .42,p.getX() + .62,p.getY() + .59);
+        StdDraw.line(p.getX() + .845,p.getY() + .52,p.getX() + .75,p.getY() + .52);
+        StdDraw.line(p.getX() + .195,p.getY() + .52,p.getX() + .29,p.getY() + .52);
     }
 
-	  public static void hor_RightMover(int i, int j) {
+	  public static void hor_RightMover(Position p) {
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledCircle(i + .52, j + .52, .40);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52, .40);
         StdDraw.setPenColor(orange);
-        StdDraw.filledCircle(i + .52,j + .52, .325);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52, .325);
         StdDraw.setPenRadius(.0145);
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.line(i + .59, j + .52,i + .45,j + .43);
-        StdDraw.line(i + .59, j + .52,i + .45,j + .62);
-        StdDraw.line(i + .845,j + .52,i + .75,j + .52);
-        StdDraw.line(i + .195,j + .52,i + .29,j + .52);
+        StdDraw.line(p.getX() + .59, p.getY() + .52,p.getX() + .45,p.getY() + .43);
+        StdDraw.line(p.getX() + .59, p.getY() + .52,p.getX() + .45,p.getY() + .62);
+        StdDraw.line(p.getX() + .845,p.getY() + .52,p.getX() + .75,p.getY() + .52);
+        StdDraw.line(p.getX() + .195,p.getY() + .52,p.getX() + .29,p.getY() + .52);
     }
 
-	  public static void hor_LeftMover(int i, int j) {
+	  public static void hor_LeftMover(Position p) {
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledCircle(i + .52,j + .52 ,.40);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52 ,.40);
         StdDraw.setPenColor(purple);
-        StdDraw.filledCircle(i + .52,j + .52, .325);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52, .325);
         StdDraw.setPenRadius(.0145);
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.line(i + .450,j + .52,i + .59,j + .43);
-        StdDraw.line(i + .450,j + .52,i + .59,j + .62);
-        StdDraw.line(i + .845,j + .52,i + .75,j + .52);
-        StdDraw.line(i + .195,j + .52,i + .29,j + .52);
+        StdDraw.line(p.getX() + .450,p.getY() + .52,p.getX() + .59,p.getY() + .43);
+        StdDraw.line(p.getX() + .450,p.getY() + .52,p.getX() + .59,p.getY() + .62);
+        StdDraw.line(p.getX() + .845,p.getY() + .52,p.getX() + .75,p.getY() + .52);
+        StdDraw.line(p.getX() + .195,p.getY() + .52,p.getX() + .29,p.getY() + .52);
     }
 
-	  public static void open_Key(int i,int j) {
+	  public static void open_Key(Position p) {
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledCircle(i + .52,j + .52,.40);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.40);
         StdDraw.setPenColor(blue);
-        StdDraw.filledCircle(i + .52,j + .52,.325);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.325);
         StdDraw.setPenRadius(.0135);
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.line(i + .40,j + .52,i + .75,j + .52);
+        StdDraw.line(p.getX() + .40,p.getY() + .52,p.getX() + .75,p.getY() + .52);
         StdDraw.setPenRadius(.010);
-        StdDraw.line(i + .7,j + .52,i + .7,j + .39);
-        StdDraw.line(i + .6,j + .52,i + .6,j + .39);
-        StdDraw.filledCircle(i + .365,j + .52, .125);
+        StdDraw.line(p.getX() + .7,p.getY() + .52,p.getX() + .7,p.getY() + .39);
+        StdDraw.line(p.getX() + .6,p.getY() + .52,p.getX() + .6,p.getY() + .39);
+        StdDraw.filledCircle(p.getX() + .365,p.getY() + .52, .125);
         StdDraw.setPenColor(blue);
-        StdDraw.filledCircle(i + .365,j + .52, .065);
+        StdDraw.filledCircle(p.getX() + .365,p.getY() + .52, .065);
     }
 
-	  public static void open_Port(int i, int j) {
+	  public static void open_Port(Position p) {
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledCircle(i + .52,j + .52,.40);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.40);
         StdDraw.setPenColor(StdDraw.RED);
-        StdDraw.filledCircle(i + .52,j + .52,.325);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.325);
         StdDraw.setPenRadius(.0145);
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.line(i + .36,j + .35,i + .67,j + .67);
-        StdDraw.line(i + .36,j + .67,i + .67,j + .35);
+        StdDraw.line(p.getX() + .36,p.getY() + .35,p.getX() + .67,p.getY() + .67);
+        StdDraw.line(p.getX() + .36,p.getY() + .67,p.getX() + .67,p.getY() + .35);
     }
 
-	  public static void open_VertSwitch(int i, int j) {
+	  public static void open_VertSwitch(Position p) {
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledCircle(i + .52,j + .52,.40);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.40);
         StdDraw.setPenColor(StdDraw.CYAN);
-        StdDraw.filledCircle(i + .52,j + .52,.325);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.325);
         StdDraw.setPenRadius(0.0145);
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.line(i + .52,j + .845,i + .52,j + .75);
-        StdDraw.line(i + .52,j + .195,i + .52,j + .29);
+        StdDraw.line(p.getX() + .52,p.getY() + .845,p.getX() + .52,p.getY() + .75);
+        StdDraw.line(p.getX() + .52,p.getY() + .195,p.getX() + .52,p.getY() + .29);
     }
 
-    public static void open_HorSwitch(int i,int j) {
+    public static void open_HorSwitch(Position p) {
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledCircle(i + .52,j + .52,.40);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.40);
         StdDraw.setPenColor(StdDraw.CYAN);
-        StdDraw.filledCircle(i + .52,j + .52,.325);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.325);
         StdDraw.setPenRadius(.0145);
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.line(i + .845,j + .52,i + .75,j + .52);
-        StdDraw.line(i + .195,j + .52,i + .29,j + .52);
+        StdDraw.line(p.getX() + .845,p.getY() + .52,p.getX() + .75,p.getY() + .52);
+        StdDraw.line(p.getX() + .195,p.getY() + .52,p.getX() + .29,p.getY() + .52);
     }
 
-	  public static void closed_HorSwitch(int i,int j) {
+	  public static void closed_HorSwitch(Position p) {
         StdDraw.setPenColor(StdDraw.GRAY);
-        StdDraw.filledCircle(i + .52,j + .52,.40);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.40);
         StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
-        StdDraw.filledCircle(i + .52,j + .52,.325);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.325);
         StdDraw.setPenRadius(.0145);
         StdDraw.setPenColor(StdDraw.GRAY);
-        StdDraw.line(i + .845,j + .52,i + .75,j + .52);
-        StdDraw.line(i + .195,j + .52,i + .29,j + .52);
+        StdDraw.line(p.getX() + .845,p.getY() + .52,p.getX() + .75,p.getY() + .52);
+        StdDraw.line(p.getX() + .195,p.getY() + .52,p.getX() + .29,p.getY() + .52);
     }
 
-	  public static void closed_VertSwitch(int i, int j) {
+	  public static void closed_VertSwitch(Position p) {
         StdDraw.setPenColor(StdDraw.GRAY);
-        StdDraw.filledCircle(i + .52,j + .52,.40);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.40);
         StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
-        StdDraw.filledCircle(i + .52,j + .52,.325);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.325);
         StdDraw.setPenRadius(.0145);
         StdDraw.setPenColor(StdDraw.GRAY);
-        StdDraw.line(i + .52,j + .845,i + .52,j + .75);
-        StdDraw.line(i + .52,j + .195,i + .52,j + .29);
+        StdDraw.line(p.getX() + .52,p.getY() + .845,p.getX() + .52,p.getY() + .75);
+        StdDraw.line(p.getX() + .52,p.getY() + .195,p.getX() + .52,p.getY() + .29);
     }
 
-	  public static void closed_Key(int i,int j) {
+	  public static void closed_Key(Position p) {
         StdDraw.setPenColor(StdDraw.GRAY);
-        StdDraw.filledCircle(i + .52,j + .52,.40);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.40);
         StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
-        StdDraw.filledCircle(i + .52,j + .52,.325);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.325);
         StdDraw.setPenRadius(.0135);
         StdDraw.setPenColor(StdDraw.GRAY);
-        StdDraw.line(i + .40,j + .52,i + .75,j + .52);
+        StdDraw.line(p.getX() + .40,p.getY() + .52,p.getX() + .75,p.getY() + .52);
         StdDraw.setPenRadius(.010);
-        StdDraw.line(i + .7,j + .52,i + .7,j + .39);
-        StdDraw.line(i + .6,j + .52,i + .6,j + .39);
-        StdDraw.filledCircle(i + .365,j + .52,.125);
+        StdDraw.line(p.getX() + .7,p.getY() + .52,p.getX() + .7,p.getY() + .39);
+        StdDraw.line(p.getX() + .6,p.getY() + .52,p.getX() + .6,p.getY() + .39);
+        StdDraw.filledCircle(p.getX() + .365,p.getY() + .52,.125);
         StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
-        StdDraw.filledCircle(i + .365,j + .52,.065);
+        StdDraw.filledCircle(p.getX() + .365,p.getY() + .52,.065);
     }
 
-	  public static void wall(int i, int j) {
+	  public static void wall(Position p) {
         StdDraw.setPenColor(StdDraw.BLACK);
         StdDraw.setPenRadius(.0145);
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledCircle(.52 + i,.52 + j,.40);
+        StdDraw.filledCircle(.52 + p.getX() ,.52 + p.getY() ,.40);
         StdDraw.setPenColor(StdDraw.GRAY);
-        StdDraw.filledCircle(.52 + i,.52 + j,.325);
+        StdDraw.filledCircle(.52 + p.getX() ,.52 + p.getY() ,.325);
     }
 
-  	public static void vert_LeftMover(int i, int j) {
+  	public static void vert_LeftMover(Position p) {
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledCircle(i + .52,j + .52,.40);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.40);
         StdDraw.setPenColor(purple);
-        StdDraw.filledCircle(i + .52,j + .52,.325);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.325);
         StdDraw.setPenRadius(.0145);
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.line(i + .45,j + .52,i + .59,j + .43);
-        StdDraw.line(i + .45,j + .52,i + .59,j + .62);
-        StdDraw.line(i + .52,j + .845,i + .52,j + .75);
-        StdDraw.line(i + .52,j + .195,i + .52,j + .29);
+        StdDraw.line(p.getX() + .45,p.getY() + .52,p.getX() + .59,p.getY() + .43);
+        StdDraw.line(p.getX() + .45,p.getY() + .52,p.getX() + .59,p.getY() + .62);
+        StdDraw.line(p.getX() + .52,p.getY() + .845,p.getX() + .52,p.getY() + .75);
+        StdDraw.line(p.getX() + .52,p.getY() + .195,p.getX() + .52,p.getY() + .29);
     }
 
-	  public static void vert_RightMover(int i, int j) {
+	  public static void vert_RightMover(Position p) {
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledCircle(i + .52,j + .52,.40);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.40);
         StdDraw.setPenColor(orange);
-        StdDraw.filledCircle(i + .52,j + .52,.325);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.325);
         StdDraw.setPenRadius(.0145);
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.line(i + .59,j + .52,i + .45,j + .43);
-        StdDraw.line(i + .59,j + .52,i + .45,j + .62);
-        StdDraw.line(i + .52,j + .845,i + .52,j + .75);
-        StdDraw.line(i + .52,j + .195,i + .52,j + .29);
+        StdDraw.line(p.getX() + .59,p.getY() + .52,p.getX() + .45,p.getY() + .43);
+        StdDraw.line(p.getX() + .59,p.getY() + .52,p.getX() + .45,p.getY() + .62);
+        StdDraw.line(p.getX() + .52,p.getY() + .845,p.getX() + .52,p.getY() + .75);
+        StdDraw.line(p.getX() + .52,p.getY() + .195,p.getX() + .52,p.getY() + .29);
     }
 
-	  public static void vert_DownMover(int i, int j) {
+	  public static void vert_DownMover(Position p) {
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledCircle(i + .52,j + .52,.40);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.40);
         StdDraw.setPenColor(darkOrange);
-        StdDraw.filledCircle(i + .52,j  + .52,.325);
+        StdDraw.filledCircle(p.getX() + .52,p.getY() + .52,.325);
         StdDraw.setPenRadius(.0145);
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.line(i + .52,j + .845,i + .52,j + .75);
-        StdDraw.line(i + .52,j + .195,i + .52,j + .29);
-        StdDraw.line(i + .52,j + .450,i + .40,j + .59);
-        StdDraw.line(i + .52,j + .450,i + .64,j + .59);
+        StdDraw.line(p.getX() + .52,p.getY() + .845,p.getX() + .52,p.getY() + .75);
+        StdDraw.line(p.getX() + .52,p.getY() + .195,p.getX() + .52,p.getY() + .29);
+        StdDraw.line(p.getX() + .52,p.getY() + .450,p.getX() + .40,p.getY() + .59);
+        StdDraw.line(p.getX() + .52,p.getY() + .450,p.getX() + .64,p.getY() + .59);
     }
 
 	  public static void initialiseBoard(int R, int C) {
