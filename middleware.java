ff29f3d08a8695f48b3cdc3d02266eb43b32c7c1
Verb656 <codename.unathi@gmail.com>
2016-10-14 12:44:14 +0200

    Added middleware.java

---
+++ middleware.java
@@ -0,0 +1,34 @@
+public class middleware {    
+
+    public static char readKeys() {
+        char pressedKey;
+
+        while(true) {     
+            if(StdDraw.hasNextKeyTyped()) {
+                pressedKey = StdDraw.nextKeyTyped();
+                return pressedKey;
+            }
+        }
+    }
+
+    public static void parseArgs() {
+    
+      /* Fill code here */
+    
+    }
+
+    public static void writeToFile() {
+    
+      /* Fill code here */
+    
+    } 
+
+    public static void sound() {
+    
+      /* Fill code here */ 
+
+    }
+
+
+}
+
