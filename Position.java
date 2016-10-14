b1864f3969e90ba8b1b33acc147fdaa0b14a4173
Verb656 <codename.unathi@gmail.com>
2016-10-14 10:32:54 +0200

    Added a complete Position.java

--- Position.java
+++ Position.java
@@ -1,2 +1,22 @@
 public class Position {
+    private int x;
+    private int y;
+
+    public Position (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() { return x; }
+
+    public int getY() { return y; }
+
+    public void incrementX() { x = x + 1; }
+
+    public void incrementY() { y = y + 1; }
+
+    public void decrementX() { x = x - 1; }
+
+    public void decrementY() { y = y - 1; }
+
 }
