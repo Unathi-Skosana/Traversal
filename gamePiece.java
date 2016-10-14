571e5f99c9e13e7d7d238e930c89c066399a2419
Verb656 <codename.unathi@gmail.com>
2016-10-14 09:30:30 +0200

    Added skeleton methods to gamePiece.java

---
+++ gamePiece.java
@@ -0,0 +1,23 @@
+public class gamePiece {
+
+    private final String symbol;
+    private Position position;
+
+
+    public gamePiece(String symbol, Position position) {
+
+        this.position = position;
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+    
+        return symbol;
+    }
+
+    public Position getPosititon() {
+    
+        return position;
+    }
+
+}
