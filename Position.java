6b814cb84f79d565e8f2e0c6e7fd6d088252db90
Verb656 <codename.unathi@gmail.com>
2016-10-22 13:33:14 +0200

    Slight modifications

--- Position.java
+++ Position.java
@@ -6,22 +6,22 @@ public class Position {
         this.x = x;
         this.y = y;
     }
-
+    
     public int getX() { return x; }
 
     public int getY() { return y; }
 
-    public void setX(int x) { this.x = x; }
+    public void setX(int x)  { this.x = x; }
 
-    public void setY(int y) { this.y = y; }
+    public void setY(int y)  { this.y = y; }
 
-    public void incrementX() { x = x + 1; }
+    public void incrementX() { x = x + 1;  }
 
-    public void incrementY() { y = y + 1; }
+    public void incrementY() { y = y + 1;  }
 
-    public void decrementX() { x = x - 1; }
+    public void decrementX() { x = x - 1;  }
 
-    public void decrementY() { y = y - 1; }
+    public void decrementY() { y = y - 1;  }
     
     public boolean isEquals(Position p) { return (p.getX() == this.x && p.getY() == this.y); }
 
